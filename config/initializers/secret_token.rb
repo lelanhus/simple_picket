c6fc2d022b60c673e7e178ac00f19969febe6e5b
Leland Husband <leland.husband@gmail.com>
2010-11-22 02:24:08 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimplePicket::Application.config.secret_token = '3eb7ddcdc53be79654646d9d4de45d06d9f5b5055131a94e1faa1e986ff94183830dae631eae1cd2bc24c798f019fe252ce85b35d1a91dc25b4c89749ea97e76'
